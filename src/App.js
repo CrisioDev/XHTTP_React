471db7205bf119b87bc8a9700ac744fbf8456941
CrisioDev <crisioletsplay@gmail.com>
2019-03-25 14:06:13 +0100

    Basic setup with axios

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
+//Sample Project for XHTTP requests with axios
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
+}
+
+export default App;
