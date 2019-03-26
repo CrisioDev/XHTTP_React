3687349cf65b5c10ac86360b12ce7b59db2b2510
CrisioDev <crisioletsplay@gmail.com>
2019-03-26 14:20:41 +0100

    Implemented basic posting system

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-
-        );
-        return post;
+  state = {
+    selectedPost: null
+  };
+  componentDidUpdate() {
+    if (this.props.selectedPost) {
+      if (this.state.selectedPost && this.state.selectedPost.id === this.props.selectedPost.id) {
+    } else {
+        axios
+          .get(
+            "https://jsonplaceholder.typicode.com/posts/" +
+              this.props.selectedPost.id
+          )
+          .then(response => {
+            this.setState({
+              selectedPost: response.data
+            });
+          });
+      }
     }
+  }
+
+  deletePostHandler = () => {
+      axios.delete("https://jsonplaceholder.typicode.com/posts/" +
+      this.props.selectedPost.id).then(response =>{
+          console.log(response);
+      })
+     }
+
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.state.selectedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.selectedPost.title}</h1>
+          <p>{this.state.selectedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectedPost: null
+  };
+
+  componentDidMount() {
+    //Sends Get and gets data asynchronusly
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const posts = response.data.slice(0, 6);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Crisio"
+        };
+      });
+      this.setState({
+        posts: updatedPosts
+      });
+      //console.log(response["data"]);
+    });
+  }
+
+  postSelectHandler = (post) => {
+    this.setState({
+      selectedPost: post
+    });
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          title={post.title}
+          key={post.id}
+          author={post.author}
+          clicked={() => this.postSelectHandler(post)}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost 
+          selectedPost={this.state.selectedPost}
+          />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
